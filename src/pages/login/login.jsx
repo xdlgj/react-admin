90b253475eb3e0119db7b35e25aed241334926ea
xudonglin <xu.dong.lin@finsiot.com>
2019-11-06 12:56:03 +0800

    静态登陆登陆界面

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,42 @@
 import React, {Component} from 'react'
+import {Form, Icon, Input, Button} from 'antd'
+import './login.less'
+import logo from './images/logo.png'
 /*
 登录的路由组件
 */
 export default class Login extends Component {
     render(){
         return(
-            <div>Login</div>
+            <div className='login'>
+                <header className='login-header'>
+                    <img src={logo} alt='logo'></img>
+                    <h1>React项目：后台管理系统</h1>
+                </header>
+                <section className='login-content'>
+                    <h2>用户登陆</h2>
+                    <Form className='login-form' onSubmit={this.handleSubmit}>
+                        <Form.Item>
+                            <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Username"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType='submit' className='login-form-button'>
+                                Log in
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
         )
     }
 }
