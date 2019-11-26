b3dc3a05d44b76782f79d7e14b1f0d085d5144bd
xdl <13333759329@163.com>
2019-11-26 22:43:42 +0800

    添加商品选择分类

--- src/pages/product/add-update.jsx
+++ src/pages/product/add-update.jsx
@@ -2,12 +2,44 @@ import React, {Component} from 'react'
 import {Card, Icon, Form, Button, Input} from 'antd'
 import LinkButton from "../../components/link-button"
 import {withRouter} from 'react-router-dom'
+import { Cascader } from 'antd';
+import { reqCategorys} from '../../api'
 /*
 Product的添加和更新子路由组件
 */
 const {Item} = Form
 const {TextArea} = Input
 class ProductAddUpdate extends Component {
+	state = {
+		options : [],
+	}
+	loadData = async (selectedOptions) => {
+		//获取被选中的对象
+	    const targetOption = selectedOptions[selectedOptions.length - 1];
+	    //显示loading
+	    targetOption.loading = true;
+
+	    //根据选中的分类，请求获取二级分类列表
+	    const subCategorys = await this.getCategorys(targetOption.value)
+	    targetOption.loading = false;
+	    if (subCategorys && subCategorys.length>0){
+	    	//生成一个二级列表的options
+	    	const childOptions = subCategorys.map(subCategory =>({
+	    		value: subCategory._id,
+	    		label: subCategory.name,
+	    		isLeaf: true
+	    	}))
+	    	//关联到当前option上
+	    	targetOption.children = childOptions
+	    }else{// 当前选中的分类没有二级分类
+	    	targetOption.isLeaf = true
+	    }
+	   
+        this.setState({
+        	options: [...this.state.options],
+        });
+
+	};
 	/*
 	校验产品价格
 	*/
@@ -26,6 +58,34 @@ class ProductAddUpdate extends Component {
 				alert("发送ajax请求")
 			}
 		})
+	}
+	initOptions = (categorys) => {
+		//根据categorys生成options数组
+		const options = categorys.map((category)=>({
+			value: category._id,
+		    label: category.name,
+		    isLeaf: false
+		}))
+		//更新状态
+		this.setState({options})
+	}
+	/*
+	异步获取一级或二级分类列表，并显示
+	async函数的返回值是一个promise对象，promise的结果和值由async的结果来决定
+	*/
+	getCategorys = async (parentId) => {
+		const result = await reqCategorys(parentId)
+		if (result.status===0){
+			const categorys = result.data
+			if (parentId==='0'){//一级列表
+				this.initOptions(categorys)
+			}else{
+				return categorys
+			}
+		}
+	}
+	componentDidMount () {
+		this.getCategorys('0')
 	}
 	render(){
 		//指定Item布局的配置对象
@@ -71,10 +131,9 @@ class ProductAddUpdate extends Component {
 									}
 								]
 							})(
-								<TextArea placeholder='请输入商品描述' autosize={{minRows:2, maxRow:6}}/>
+								<TextArea placeholder='请输入商品描述' autoSize={{minRows:2, maxRow:6}}/>
 							)
-						}
-						
+						}	
 					</Item>
 					<Item label="商品价格:">
 						{
@@ -93,10 +152,12 @@ class ProductAddUpdate extends Component {
 								<Input type='number' placeholder='请输入商品价格' addonAfter='元' />
 							)
 						}
-						
 					</Item>
 					<Item label="商品分类:">
-						<div>商品分类</div>
+						 <Cascader
+					        options={this.state.options}
+					        loadData={this.loadData}
+					     />
 					</Item>
 					<Item label="商品图片:">
 						<div>商品图片</div>
