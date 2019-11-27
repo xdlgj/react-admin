c1ebe62f401bd25b79b99eb7c8982006ad482739
xudonglin <xu.dong.lin@finsiot.com>
2019-11-27 18:15:57 +0800

    上传图片

--- src/pages/product/pictures-wall.jsx
+++ src/pages/product/pictures-wall.jsx
@@ -16,55 +16,43 @@ function getBase64(file) {
 
 export default class PicturesWall extends Component {
   state = {
-    previewVisible: true, // 标识是否显示大图预览
-    previewImage: '',
+    previewVisible: false, // 标识是否显示大图预览Modal
+    previewImage: '', // 大图的url
     fileList: [
       {
-        uid: '-1',
-        name: 'image.png',
-        status: 'done',
+        uid: '-1', //，每个file都有自己唯一的id
+        name: 'image.png', // 图片文件名
+        status: 'done', // 图片状态， done：已经上传， uploading：状态上传，removed：已经删除
         url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      },
-      {
-        uid: '-2',
-        name: 'image.png',
-        status: 'done',
-        url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      },
-      {
-        uid: '-3',
-        name: 'image.png',
-        status: 'done',
-        url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      },
-      {
-        uid: '-4',
-        name: 'image.png',
-        status: 'done',
-        url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      },
-      {
-        uid: '-5',
-        name: 'image.png',
-        status: 'error',
-      },
+      }, 
     ],
   };
-
+  /*
+  隐藏Modal
+  */
   handleCancel = () => this.setState({ previewVisible: false });
-
+  /*
+  点击图片上面的小眼睛触发（查看大图）
+  */
   handlePreview = async file => {
     if (!file.url && !file.preview) {
       file.preview = await getBase64(file.originFileObj);
     }
-
+    //显示指定file对应的大图
     this.setState({
       previewImage: file.url || file.preview,
       previewVisible: true,
     });
   };
-
-  handleChange = ({ fileList }) => this.setState({ fileList });
+  /*
+    file: 当前操作的图片文件（上传/删除）
+    fileList：所有已上传图片文件对象数组
+  */
+  handleChange = ({file, fileList }) => {
+      console.log("aaaaaaaaa", file, fileList)
+      //在操作（上传/删除）过程中更新fileList状态
+      this.setState({fileList})
+  };
 
   render() {
     const { previewVisible, previewImage, fileList } = this.state;
@@ -77,9 +65,11 @@ export default class PicturesWall extends Component {
     return (
       <div>
         <Upload
-          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-          listType="picture-card"
-          fileList={fileList}
+          action="/manage/img/upload" //上传图片的接口地址
+          accept='image/*' //只接收图片格式
+          name='image' //请求参数名
+          listType="picture-card" //卡片样式
+          fileList={fileList} //所有已上传图片文件对象的数组
           onPreview={this.handlePreview}
           onChange={this.handleChange}
         >
